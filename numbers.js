e579cb75f624fef49273895f1f526ef291d7c51c
Oluwatobi Omotayo <tobitech@ymail.com>
2018-02-18 00:11:50 +0100

    working with numbers in javascript

---
+++ numbers.js
@@ -0,0 +1,71 @@
+/* Numbers */
+
+var number = 12;
+
+// you can do all the standard operations in JavaScript
+
+// addition
+number = number + 2;
+
+console.log(number);
+
+
+// subtraction
+number = 12 - 2;
+
+console.log(number);
+
+// multiplication
+number = 12 * 2;
+
+console.log(number);
+
+// division
+number = 12 / 2;
+
+console.log(number);
+
+// division to get decimal number
+number = 12 / 7;
+
+console.log(number);
+
+
+// modulus
+number = 12 % 7;
+
+console.log(number);
+
+// addition with decimal numbers
+number = 12 + 7.3;
+
+console.log(number);
+
+// divide with decimal numbers
+number = 12 / 7.3;
+
+console.log(number);
+
+// multiply with decimal numbers
+number = 1.2 * 7.3;
+
+console.log(number);
+
+// increment by one
+number++;
+
+// decrement by one
+number--;
+
+// 
+number = number + 5; // same as
+number += 5;
+number /= 5;
+number -= 5;
+number *= 5;
+
+console.log(number++); // logs before incrementing
+console.log(number);
+
+console.log(++number); // logs after incrementing
+console.log(number);
